5aa0f34f67eb79d91878ea3db07323cbfe99955c
eschnei2 <everettschneider@gmail.com>
2021-05-27 10:47:47 -0500

    initial push

---
+++ leonids-toys.js
@@ -0,0 +1,15 @@
+const chickenSqueeker = {
+    id: 1,
+    name: "Certified Chicken Squeek Toy",
+    maker: "Fisher Price",
+    Cost: 4.99
+}
+
+const plasticScrewdriver = {
+    id: 2,
+    name: "hard plastic toy screwdriver",
+    maker: "Genius Kids Inc.",
+    Cost: 1.99
+}
+
+const 

ef4bca3e0c47c546191a16ee6fa124d3c8de9d9a
eschnei2 <everettschneider@gmail.com>
2021-05-27 11:27:37 -0500

    console log now just shows name

--- leonids-toys.js
+++ leonids-toys.js
@@ -1,20 +1,24 @@
-const chickenSqueeker = {
-    id: 1,
-    name: "Certified Chicken Squeek Toy",
-    maker: "Fisher Price",
-    Cost: 4.99
-}
-
-const plasticScrewdriver = {
-    id: 2,
-    name: "hard plastic toy screwdriver",
-    maker: "Genius Kids Inc.",
-    Cost: 1.99
-}
-
-const lightBright = {
+const toys = [
+  {
+      id: 1,
+      name: "Certified Chicken Squeek Toy",
+      maker: "Fisher Price",
+      Cost: 4.99
+},
+  {
+      id: 2,
+      name: "hard plastic toy screwdriver",
+      maker: "Genius Kids Inc.",
+      Cost: 1.99
+  },
+  {
     id: 3,
-    name: "Light Bright",
+    name: "Light Bright Deluxe model",
     maker: "Hasbro",
     cost: 10.00
+  }
+]
+
+for (const toy of toys) {
+    console.log(toy.name)
 }
